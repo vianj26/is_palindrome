aad8f92fa27a1d75565a9b121acbbf5a0e7dbdb4
vianj26 <150534829+vianj26@users.noreply.github.com>
2024-04-20 19:16:49 +0800

    Add files via upload
    wrong first program

--- master.cpp
+++ master.cpp
@@ -1,88 +1,47 @@
 #include <iostream>
-#include <cmath>
 #include <string>
-#include <ctime>
-#include <cstdlib>
-#include <vector>
 
 using namespace std;
 
-void play_game();
-void print_list(vector<int>);
+bool is_palindrome(string);
 
 int main()
 {
-    srand(time(NULL));
-    int response;
 
-    cout << "[1]Play game.\n[2]]Quit.\n>>";
-    cin >> response;
+    string word;
 
-    switch(response)
-    {
-        case 1:
-            play_game();
-            break;
-        case 2:
-            break;
-    }
+    cout << "Enter the word to check if palindrome.\n>>";
+    getline(cin, word);
+
+    cout << (is_palindrome(word) ? "It is a palindrome." : "It is not a palindrome.") << endl;
+
+    if(word.length() == 1) cout << "One word letter is technically a palindrome." << endl;
+    if(word.length() == 0) cout << "Technically it is still a palindrome." << endl;
 
     return 0;
+
+
 }
 
-void print_list(vector<int> list)
+bool is_palindrome(string word)
 {
-    int size = list.size();
+    int start = 0;
+    int end = word.length() - 1;
+    bool result = true;
+    if (end == 0 || end == 1) return result;
+    
 
-    cout << "[";
-
-    for(int i = 0; i < size; i++)
+    while(end > start)
     {
-        cout << list[i];
-        if(size - i > 1) cout << "\t";
+        if(word[start] != word[end])
+        {
+            result = false;
+            break;
+        }
+        start++;
+        end--;
+
     }
 
-    cout << "]" << endl;
-}
-
-
-void play_game()
-{
-    char answer = 'y';
-    do
-    {
-        //Play game
-        if(answer == 'y')
-        {
-            int target = rand() % 101;
-            int score = 10;
-            int guess;
-            vector<int> guess_list;
-            cout << "Guess a number from 0 to 100.\n>>";
-            
-            
-            do
-            {
-                cin >> guess;
-
-                guess_list.push_back(guess);
-                
-                if(target > guess){cout << "Higher.\n>>";score--;}
-                else if( target < guess){cout << "Lower.\n>>";score--;}
-
-                if(score == 0){cout << "Sorry you lost.\n"; print_list(guess_list);}
-
-            } while (target != guess);
-
-            cout << "Congratulations. Your score is " << score << ".\n";
-            print_list(guess_list);
-            
-            
-        }
-
-        cout << "Do you want to play again?[y/n]\n>>";
-        cin >> answer; 
-
-        if(!(answer == 'y' || answer == 'n')){cout << "Invalide response.\n";}
-    } while (answer == 'y');
+    return result;
 }
